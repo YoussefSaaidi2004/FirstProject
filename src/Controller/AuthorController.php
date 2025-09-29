c268b98ba1bf0e550bd016dff71f557288cdbce2
YoussefSaaidi2004 <youssef.saaidi@outlook.com>
2025-09-29 19:05:44 +0100

    Second commit

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -22,4 +22,44 @@ final class AuthorController extends AbstractController
             'name' => $name,
         ]);
     }
+    #[Route('/authors/list', name: 'app_authors_list')]
+    public function listAuthors(): Response
+    {
+        $authors = [
+            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>'victor.hugo@gmail.com', 'nb_books' => 100),
+            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => 'William Shakespeare', 'email' =>'william.shakespeare@gmail.com', 'nb_books' => 200),
+            array('id' => 3, 'picture' => '/images/Taha-Hussein.jpg','username' => 'Taha Hussein', 'email' =>'taha.hussein@gmail.com', 'nb_books' => 300),
+        ];
+
+
+        return $this->render('author/list.html.twig', [
+         'authors' => $authors,
+        ]);
+    }
+
+    #[Route('/author/details/{id}', name: 'app_author_details')]
+    public function authorDetails($id): Response
+    {
+        $authors = [
+            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>'victor.hugo@gmail.com', 'nb_books' => 100),
+            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => 'William Shakespeare', 'email' =>'william.shakespeare@gmail.com', 'nb_books' => 200),
+            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>'taha.hussein@gmail.com', 'nb_books' => 300),
+        ];
+
+        $author = null;
+        foreach ($authors as $a) {
+            if ($a['id'] == $id) {
+                $author = $a;
+                break;
+            }
+        }
+
+        if (!$author) {
+            throw $this->createNotFoundException('Auteur non trouvé');
+        }
+
+        return $this->render('author/showAuthor.html.twig', [
+            'author' => $author,
+        ]);
+    }
 }
